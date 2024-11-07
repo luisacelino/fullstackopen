ffaed0da9b19b3eb169dcf1915ba0683dfe0568d
Luisa Celino <luisa.celino@xing.com>
2024-11-07 15:53:49 +0000

    Refactor App component into Header, Content, and Total components

---
+++ part1/my-app/src/App.jsx
@@ -0,0 +1,37 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <p key={index}>{part.name} {part.exercises}</p>
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0)
+  return <p>Number of exercises {totalExercises}</p>
+}
+
+export default App
